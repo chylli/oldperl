482d24d6b4472c9d6c1f8a3e6ccfb08c5adc10e9
Chylli <chylli@binary.com>
2016-05-27 19:53:28 +0800

    typo

--- test.sh
+++ test.sh
@@ -1,9 +1,10 @@
+#/bin/bash
 for ((i=1; i<=10; i++))
 do
     (cd ../cpan && git checkout master)
     PERL=/usr/bin/perl
-    perl -v
-    /usr/bin/perl tp.pl 1 >/dev/null
+    $PERL -v
+    $PERL tp.pl 1 >/dev/null
     echo time (/usr/bin/perl tp.pl > /dev/null)
 
     (cd ../cpan && git checkout chylli/v5.22.2)
