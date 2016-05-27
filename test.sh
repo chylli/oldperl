f0e935877fb7d4336405e98a41e56457cf20e190
Chylli <chylli@binary.com>
2016-05-27 19:59:21 +0800

    try

--- test.sh
+++ test.sh
@@ -3,22 +3,22 @@ for ((i=1; i<=10; i++))
 do
     (cd ../cpan && git checkout master)
     PERL=/usr/bin/perl
-    $PERL -v
+    $PERL -e 'print $^V'
     $PERL tp.pl 1 >/dev/null
-    echo "time ($PERL tp.pl > /dev/null)"
+    time ($PERL tp.pl > /dev/null)
 
     (cd ../cpan && git checkout chylli/v5.22.2)
     (cd ../perl && git checkout chylli/v5.22.2)
     PERL=/home/git/regentmarkets/perl/bin/perl
-    $PERL -v
+    $PERL -e 'print $^V'
     $PERL tp.pl 1 >/dev/null
-    echo "time ($PERL tp.pl > /dev/null)"
+    time ($PERL tp.pl > /dev/null)
 
     (cd ../cpan && git checkout chylli/v5.24.0)
     (cd ../perl && git checkout chylli/v5.24.0)
     PERL=/home/git/regentmarkets/perl/bin/perl
-    $PERL -v
+    $PERL -e 'print $^V'
     $PERL tp.pl 1 >/dev/null
-    echo "time ($PERL tp.pl > /dev/null)"
+    time ($PERL tp.pl > /dev/null)
 
 done
