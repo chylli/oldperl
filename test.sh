6d6b7b4409aafaed4826497d35f04331a5d369ed
Chylli <chylli.email@gmail.com>
2016-05-29 01:15:11 +0000

    add test.sh

---
+++ test.sh
@@ -0,0 +1,24 @@
+#/bin/bash
+for ((i=1; i<=10; i++))
+do
+    (cd ../cpan && git checkout master)
+    PERL=/usr/bin/perl
+    $PERL -e 'print "$^V\n"'
+    $PERL tp.pl 1 >/dev/null 2>&1 && echo "ok"
+    time ($PERL tp.pl > /dev/null 2>&1)
+
+    (cd ../cpan && git checkout chylli/v5.22.2)
+    (cd ../perl && git checkout chylli/v5.22.2)
+    PERL=/home/git/regentmarkets/perl/bin/perl
+    $PERL -e 'print "$^V\n"'
+    $PERL tp.pl 1 >/dev/null 2>&1 && echo "ok"
+    time ($PERL tp.pl > /dev/null 2>&1)
+
+    (cd ../cpan && git checkout chylli/v5.24.0)
+    (cd ../perl && git checkout chylli/v5.24.0)
+    PERL=/home/git/regentmarkets/perl/bin/perl
+    $PERL -e 'print "$^V\n"'
+    $PERL tp.pl 1 >/dev/null 2>&1 &&  echo "ok"
+    time ($PERL tp.pl > /dev/null 2>&1)
+
+done
