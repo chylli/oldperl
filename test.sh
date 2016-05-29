92907ed1ebfad7ab684c7736e76c5cf3cd52a7fb
Chylli <chylli.email@gmail.com>
2016-05-29 01:17:15 +0000

    update test.sh

--- test.sh
+++ test.sh
@@ -4,21 +4,21 @@ do
     (cd ../cpan && git checkout master)
     PERL=/usr/bin/perl
     $PERL -e 'print "$^V\n"'
-    $PERL tp.pl 1 >/dev/null 2>&1
+    $PERL tp.pl 1 >/dev/null 2>&1 && echo "ok"
     time ($PERL tp.pl > /dev/null 2>&1)
 
     (cd ../cpan && git checkout chylli/v5.22.2)
     (cd ../perl && git checkout chylli/v5.22.2)
     PERL=/home/git/regentmarkets/perl/bin/perl
     $PERL -e 'print "$^V\n"'
-    $PERL tp.pl 1 >/dev/null 2>&1
+    $PERL tp.pl 1 >/dev/null 2>&1 && echo "ok"
     time ($PERL tp.pl > /dev/null 2>&1)
 
     (cd ../cpan && git checkout chylli/v5.24.0)
     (cd ../perl && git checkout chylli/v5.24.0)
     PERL=/home/git/regentmarkets/perl/bin/perl
     $PERL -e 'print "$^V\n"'
-    $PERL tp.pl 1 >/dev/null 2>&1
+    $PERL tp.pl 1 >/dev/null 2>&1 &&  echo "ok"
     time ($PERL tp.pl > /dev/null 2>&1)
 
 done
